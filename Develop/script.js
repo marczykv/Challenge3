ac224f065fa9e2dd69206cf327b5c56a972cb30e
Victor Marczyk <marczykv@gmail.com>
2023-06-20 21:27:29 -0400

    created the function of the password generator, added character validation

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,73 @@
-// Assignment code here
+// Added the function of the password generator, using check boxes through html to supplement prompts 
+
+function generatePassword() {
+
+  var characterLength = document.querySelector('#characterLengthInput').value;
+  var upperCaseCheckInput = document.querySelector('#upperCaseCheck').checked;
+  var lowerCaseCheck = document.querySelector('#lowerCaseCheck').checked;
+  var specialCaseCheck = document.querySelector('#specialCaseCheck').checked;
+  var numberCheck = document.querySelector('#numberCheck').checked;
+
+  var password = "";
+  for (var i = 0; i < characterLength; i++) {
+    var validCharacter = false;
+    var character;
+
+    // Added if and else if statements to determine if the character generated is valid 
+    while (validCharacter == false) {
+      character = randomizedValue();
+
+      if (!lowerCaseCheck && lowerCharacters.indexOf(character) !== -1) {
+        validCharacter = false;
+      }
+      else if (!upperCaseCheckInput && upperCharacters.indexOf(character) !== -1) {
+        validCharacter = false;
+      }
+      else if (!specialCaseCheck && specialCharacters.indexOf(character) !== -1) {
+        validCharacter = false
+      }
+      else if (!numberCheck && numbers.indexOf(character) !== -1) {
+        validCharacter = false;
+      }
+      else {
+        validCharacter = true;
+      }
+    }
+
+    password += character;
+  }
+
+  return password;
+}
+// Added in the characters that would appear in the generated password for each specific character type 
+var specialCharacters = ['%', '!', '$', '@', '#']
+var lowerCharacters = ['a', 'b', 'c', 'd', 'e', 'f'];
+var upperCharacters = ['A', 'B', 'C', 'D', 'E', 'F'];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var allChars = [...lowerCharacters,
+...upperCharacters,
+...numbers,
+...specialCharacters];
+function randomizedValue() {
+
+  var index = Math.floor(Math.random() * allChars.length);
+  var character = allChars[index];
+
+  return character;
+}
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+  if (password == "") {
+
+    alert("You must enter a value for character length.")
+  }
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
